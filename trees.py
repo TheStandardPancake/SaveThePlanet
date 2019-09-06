ca96901140747e9f4c4c43ceb3324f8872c77d8d
TheStandardPancake <40510902+TheStandardPancake@users.noreply.github.com>
2019-09-06 17:18:50 +1000

    not bad, 80 trees a day

--- trees.py
+++ trees.py
@@ -1,13 +1,11 @@
-from slenium import webdriver
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 driver = webdriver.Chrome('./chromedriver.exe')
-driver.get('https://www.ecosia.org')
-i=1
 while True:
-    search_bar = driver.find_element_by_type('search')
+    driver.get('https://www.ecosia.org')
+    search_bar = driver.find_element_by_name('q')
     search_bar.send_keys('I would like to comission a tree planted')
-    search_bar.send_keys(Key.RETURN)
-    sleep(1)
-    print(i)
-    i+=1
+    button = driver.find_element_by_xpath('//*[@id="__layout"]/div/section/div[1]/form/div[2]/button[2]')
+    button.click()
+    sleep(24)
